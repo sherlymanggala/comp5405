6d7b5922326c7d692509aa14be4ed6580c8b7518
sherlymanggala <sherly_manggala@yahoo.com>
2019-04-24 21:58:43 +1000

    added logo and version text on splash activity

---
+++ app/src/main/java/com/example/comp5405/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.comp5405;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        },4*1000); // 4 seconds delay
+    }
+}
